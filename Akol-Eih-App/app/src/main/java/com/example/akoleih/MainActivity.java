19801082a9b02226c1b53d9e24252397e636996c
Patrick Atef <patrickatefffa22@gmail.com>
2025-04-23 17:09:58 +0200

    UI updates for better view (added some drawables + button animator xml)

--- Akol-Eih-App/app/src/main/java/com/example/akoleih/MainActivity.java
+++ Akol-Eih-App/app/src/main/java/com/example/akoleih/MainActivity.java
@@ -1,12 +1,10 @@
-package com.example.akoleih;
+package com.example.akoleih.auth.view;
 
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import com.example.akoleih.R;
 
 public class MainActivity extends AppCompatActivity {
 
