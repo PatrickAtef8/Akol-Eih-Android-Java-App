d7c8c8052a5762b423579c6885accdc3ab4d8a33
Patrick Atef <patrickatefffa22@gmail.com>
2025-04-27 16:19:55 +0300

    Home Fragment Added to my home pkg include Random Meal + Meal Categories

--- Akol-Eih-App/app/src/main/java/com/example/akoleih/home/view/HomeFragment.java
+++ Akol-Eih-App/app/src/main/java/com/example/akoleih/home/view/HomeFragment.java
@@ -1,66 +1,92 @@
 package com.example.akoleih.home.view;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import com.example.akoleih.R;
+import com.example.akoleih.home.adapter.CategoriesAdapter;
+import com.example.akoleih.home.adapter.RandomMealAdapter;
+import com.example.akoleih.home.contract.HomeContract;
+import com.example.akoleih.home.model.Category;
+import com.example.akoleih.home.model.Meal;
+import com.example.akoleih.home.network.RetrofitClient;
+import com.example.akoleih.home.presenter.HomePresenter;
+import com.example.akoleih.home.model.repository.HomeRepositoryImpl;
+import com.example.akoleih.home.network.api.CategoriesRemoteDataSource;
+import com.example.akoleih.home.network.api.MealRemoteDataSource;
+import java.util.List;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link HomeFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class HomeFragment extends Fragment {
+public class HomeFragment extends Fragment implements HomeContract.View {
+    private HomePresenter presenter;
+    private CategoriesAdapter categoriesAdapter;
+    private RandomMealAdapter randomMealAdapter;
+    private RecyclerView categoriesRecyclerView;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+        CategoriesRemoteDataSource categoriesRemoteDataSource = RetrofitClient.getInstance()
+                .create(CategoriesRemoteDataSource.class);
+        MealRemoteDataSource mealRemoteDataSource = RetrofitClient.getInstance()
+                .create(MealRemoteDataSource.class);
 
-    public HomeFragment() {
-        // Required empty public constructor
+        presenter = new HomePresenter(this, new HomeRepositoryImpl(categoriesRemoteDataSource, mealRemoteDataSource));
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment HomeFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static HomeFragment newInstance(String param1, String param2) {
-        HomeFragment fragment = new HomeFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        categoriesRecyclerView = view.findViewById(R.id.categories_recycler_view);
+        categoriesRecyclerView.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false));
+        categoriesAdapter = new CategoriesAdapter(null);
+        categoriesRecyclerView.setAdapter(categoriesAdapter);
+
+        RecyclerView randomMealRecyclerView = view.findViewById(R.id.random_meal_recycler_view);
+        randomMealRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        randomMealAdapter = new RandomMealAdapter();
+        randomMealRecyclerView.setAdapter(randomMealAdapter);
+
+        return view;
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        presenter.getCategories();
+        presenter.getRandomMeal();
+    }
+
+    @Override
+    public void showCategories(List<Category> categories) {
+        if (categoriesAdapter == null) {
+            categoriesAdapter = new CategoriesAdapter(categories);
+            categoriesRecyclerView.setAdapter(categoriesAdapter);
+        } else {
+            categoriesAdapter.updateCategories(categories);
         }
     }
+    @Override
+    public void showRandomMeal(Meal meal) {
+        randomMealAdapter.setMeal(meal);
+    }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+    public void showError(String message) {
+        //  with a Toast or Snackbar
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        presenter.onDestroy();
     }
 }
