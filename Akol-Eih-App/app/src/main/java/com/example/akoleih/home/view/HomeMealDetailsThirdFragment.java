5b3a4939e748198eaa12ebeffc4f46b7604ac573
Patrick Atef <patrickatefffa22@gmail.com>
2025-05-03 16:42:04 +0300

    Added Video Fragment in home feature on the meal details fragment to let the user when click on the watch video button to open the video in a dialog

--- Akol-Eih-App/app/src/main/java/com/example/akoleih/home/view/HomeMealDetailsThirdFragment.java
+++ Akol-Eih-App/app/src/main/java/com/example/akoleih/home/view/HomeMealDetailsThirdFragment.java
@@ -1,4 +1,3 @@
-// MealDetailsFragment.java
 package com.example.akoleih.home.view;
 
 import android.os.Bundle;
@@ -6,28 +5,33 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import com.bumptech.glide.Glide;
 import com.example.akoleih.R;
-import com.example.akoleih.home.contract.HomeContract;
+import com.example.akoleih.home.adapter.IngredientAdapter;
 import com.example.akoleih.home.model.Category;
 import com.example.akoleih.home.model.Meal;
-import com.example.akoleih.home.presenter.HomePresenter;
-import com.example.akoleih.home.network.RetrofitClient;
+import com.example.akoleih.home.presenter.HomePresenterImpl;
 import com.example.akoleih.home.model.repository.HomeRepositoryImpl;
-import com.example.akoleih.home.network.api.CategoriesRemoteDataSource;
-import com.example.akoleih.home.network.api.MealRemoteDataSource;
+import com.example.akoleih.home.network.MealRemoteDataSourceImpl;
+import com.google.android.material.button.MaterialButton;
 import com.google.android.material.imageview.ShapeableImageView;
-
 import java.util.List;
 
-public class HomeMealDetailsThirdFragment extends Fragment implements HomeContract.View {
-    private HomePresenter presenter;
+public class HomeMealDetailsThirdFragment extends Fragment implements HomeView {
+    private HomePresenterImpl presenter;
     private String mealId;
     private ShapeableImageView mealImage;
-    private TextView mealName, mealArea, mealInstructions, mealIngredients;
+    private TextView mealName, mealArea, mealInstructions;
+    private RecyclerView ingredientsRecyclerView;
+    private IngredientAdapter ingredientsAdapter;
+    private MaterialButton watchButton;
+    private String youtubeUrl;
 
     public static HomeMealDetailsThirdFragment newInstance(String mealId) {
         HomeMealDetailsThirdFragment fragment = new HomeMealDetailsThirdFragment();
@@ -43,25 +47,27 @@ public class HomeMealDetailsThirdFragment extends Fragment implements HomeContra
         if (getArguments() != null) {
             mealId = getArguments().getString("mealId");
         }
-
-        CategoriesRemoteDataSource categoriesRemoteDataSource = RetrofitClient.getInstance()
-                .create(CategoriesRemoteDataSource.class);
-        MealRemoteDataSource mealRemoteDataSource = RetrofitClient.getInstance()
-                .create(MealRemoteDataSource.class);
-
-        presenter = new HomePresenter(this, new HomeRepositoryImpl(categoriesRemoteDataSource, mealRemoteDataSource));
+        presenter = new HomePresenterImpl(this,
+                new HomeRepositoryImpl(null, new MealRemoteDataSourceImpl()));
     }
 
     @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_meal_details, container, false);
 
         mealImage = view.findViewById(R.id.meal_image);
         mealName = view.findViewById(R.id.meal_name);
         mealArea = view.findViewById(R.id.meal_area);
         mealInstructions = view.findViewById(R.id.meal_instructions);
-        mealIngredients = view.findViewById(R.id.meal_ingredients);
+        watchButton = view.findViewById(R.id.watch);
+
+        ingredientsRecyclerView = view.findViewById(R.id.ingredients_recycler_view);
+        ingredientsRecyclerView.setLayoutManager(new LinearLayoutManager(
+                getContext(), LinearLayoutManager.HORIZONTAL, false));
+        ingredientsAdapter = new IngredientAdapter();
+        ingredientsRecyclerView.setAdapter(ingredientsAdapter);
 
         return view;
     }
@@ -74,27 +80,43 @@ public class HomeMealDetailsThirdFragment extends Fragment implements HomeContra
 
     @Override
     public void showMealDetails(Meal meal) {
-        Glide.with(this)
-                .load(meal.getThumbnail())
-                .placeholder(R.drawable.foodloading)
-                .error(R.drawable.foodloading)
-                .into(mealImage);
+        if (getContext() != null) {
+            Glide.with(this)
+                    .load(meal.getThumbnail())
+                    .placeholder(R.drawable.foodloading)
+                    .error(R.drawable.foodloading)
+                    .into(mealImage);
 
-        mealName.setText(meal.getName());
-        mealArea.setText(meal.getArea());
-        mealInstructions.setText(meal.getInstructions());
-        mealIngredients.setText(meal.getIngredients());
+            mealName.setText(meal.getName());
+            mealArea.setText(meal.getArea());
+            mealInstructions.setText(meal.getInstructions());
+            youtubeUrl = meal.getYoutubeUrl();
+
+            ingredientsAdapter.updateIngredients(meal.getIngredientList());
+
+            watchButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (youtubeUrl != null && !youtubeUrl.isEmpty()) {
+                        VideoFragment videoFragment = VideoFragment.newInstance(youtubeUrl);
+                        videoFragment.show(getParentFragmentManager(), "video_dialog");
+                    } else {
+                        showError("No video available for this meal");
+                    }
+                }
+            });
+        }
     }
+    @Override public void showCategories(List<Category> categories) {}
+    @Override public void showRandomMeal(Meal meal) {}
+    @Override public void showMealsByCategory(List<Meal> meals) {}
 
-    // Implement other required methods from HomeContract.View
     @Override
-    public void showCategories(List<Category> categories) {}
-    @Override
-    public void showRandomMeal(Meal meal) {}
-    @Override
-    public void showMealsByCategory(List<Meal> meals) {}
-    @Override
-    public void showError(String message) {}
+    public void showError(String message) {
+        if (getContext() != null) {
+            Toast.makeText(getContext(), message, Toast.LENGTH_SHORT).show();
+        }
+    }
 
     @Override
     public void onDestroy() {
