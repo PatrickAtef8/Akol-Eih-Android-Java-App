ed8eb65b6a36587dd1f7170216abc1b8fbbdc567
Patrick Atef <patrickatefffa22@gmail.com>
2025-05-03 16:32:17 +0300

    Added HomeView interface to be implemented by fragments(in plan to divide the interface functionality related to each fragment to not let a class implement empty methods)

---
+++ Akol-Eih-App/app/src/main/java/com/example/akoleih/home/view/HomeView.java
@@ -0,0 +1,15 @@
+// HomeView.java
+package com.example.akoleih.home.view;
+
+import com.example.akoleih.home.model.Category;
+import com.example.akoleih.home.model.Meal;
+
+import java.util.List;
+
+public interface HomeView {
+    void showCategories(List<Category> categories);
+    void showRandomMeal(Meal meal);
+    void showMealsByCategory(List<Meal> meals);
+    void showMealDetails(Meal meal);
+    void showError(String message);
+}
